53f5e92afc77d787d3355605b0b882d50d15e928
muraviev-aa <muraviev-aa@yandex.ru>
2022-08-26 10:31:15 +0300

    26.08.2022

---
+++ Lab5/Excercise1/ITMO.CSCourse2022/ITMO.CSCourse2022.Excercise1/FileDetails.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;             // FileStream, FileReader
+
+class FileDetails
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine(args.Length);
+        //foreach(string arg in args)
+        //    Console.WriteLine(arg);
+        string fileName = args[0];
+        FileStream stream = new FileStream(fileName, FileMode.Open);
+        StreamReader reader = new StreamReader(stream);
+        int size = (int)stream.Length;
+        char[] contents = new char[size];
+
+        for(int i = 0; i < size; i++)
+        {
+            contents[i] = (char)reader.Read();
+        }
+
+        foreach(char c in contents)
+        {
+            Console.WriteLine(c);
+        }
+        reader.Close();
+
+        Summarize(contents);
+    }
+    static void Summarize(char[] contents)
+    {
+        int vowels = 0, consonants = 0, lines = 0;
+        foreach (char current in contents)
+        {
+            if (Char.IsLetter(current))
+            {
+                if ("AEIOUaeiou".IndexOf(current) != -1)
+                {
+                    vowels++;
+                }
+                else
+                {
+                    consonants++;
+                }
+            }
+            else if (current == '\n')
+            {
+                lines++;
+            }
+        }
+        Console.WriteLine("Total no characters: {0}", contents.Length);
+        Console.WriteLine("Total no vowels: {0}", vowels);
+        Console.WriteLine("Total no consonants: {0}", consonants);
+        Console.WriteLine("Total no lines: {0}", lines);
+    }
+
+    
+}
